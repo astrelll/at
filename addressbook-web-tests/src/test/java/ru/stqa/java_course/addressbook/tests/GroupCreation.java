521ebf88793ffa308fc5a227b9476a828bafde8e
astrelll <astrel100500@yandex.ru>
2022-07-14 09:16:45 +0300

    добавлено сравнение списков групп с помощью лямбда функций и компаратора

--- addressbook-web-tests/src/test/java/ru/stqa/java_course/addressbook/tests/GroupCreation.java
+++ addressbook-web-tests/src/test/java/ru/stqa/java_course/addressbook/tests/GroupCreation.java
@@ -5,6 +5,7 @@ import org.testng.annotations.*;
 import ru.stqa.java_course.addressbook.appmanager.GroupHelper;
 import ru.stqa.java_course.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -15,19 +16,14 @@ public class GroupCreation extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
 
     List<GroupData> before = app.getGroupHelper().getGroupList();
-    GroupData group = new GroupData("test88", null, null);
+    GroupData group = new GroupData("test2", null, null);
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
-
-    int max = 0;
-    for (GroupData g : after) {
-      if(g.getId() > max){
-        max = g.getId();
-      }
-    }
-    group.setId(max);
     before.add(group);
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
+    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before, after);
   }
 }
